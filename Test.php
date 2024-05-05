f05f2a1d3a42f88473cb12778dec7927c47cc7c4
ShailiManamperi <shailimanamperi2002@gmail.com>
2024-05-05 22:52:41 +0530

    update with new test

--- Test.php
+++ Test.php
@@ -1,4 +1,9 @@
 <?php
-$txt= "Php";
-echo "I Love $txt";
+echo "Hello World<br>";
+
+/*all three echo statements below are equal and legal:*/
+
+ECHO "Hello World!<br>";
+echo "Hello World!<br>";
+EcHo "Hello World!<br>";
 ?>
