97c2a231e366e512dc7c55123855362de3ee44e0
ShailiManamperi <shailimanamperi2002@gmail.com>
2024-05-05 23:03:40 +0530

    update with new test2

--- Test.php
+++ Test.php
@@ -1,9 +1,18 @@
 <?php
-echo "Hello World<br>";
+echo "Hello World<br><br>";
 
 /*all three echo statements below are equal and legal:*/
 
 ECHO "Hello World!<br>";
 echo "Hello World!<br>";
-EcHo "Hello World!<br>";
+EcHo "Hello World!<br><br>";
+
+/*all variable names are case-sensitive!*/
+$color = "red";
+$COLOR = "white";
+$coLOR =  "blue";
+echo "My car is " . $color . "<br>";
+echo "My house is " . $COLOR . "<br>";
+echo "My boat is " . $coLOR . "<br>";
+
 ?>
