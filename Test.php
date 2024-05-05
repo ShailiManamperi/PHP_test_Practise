918f9a3f87f3295b45f42dd646bff3b9950eec6f
ShailiManamperi <shailimanamperi2002@gmail.com>
2024-05-06 00:08:07 +0530

    update php object test

--- Test.php
+++ Test.php
@@ -71,4 +71,21 @@ function myTest3() {
 myTest3();
 myTest3();
 myTest3();
+
+/*PHP Object*/
+class Car {
+    public $color;
+    public $model;
+    public function __construct($color, $model) {
+        $this->color = $color;
+        $this->model = $model;
+    }
+    public function message() {
+        return "My car is a " . $this->color . " " . $this->model . "!";
+    }
+}
+
+$myCar = new Car("red", "Volvo");
+var_dump($myCar);
+
 ?>
